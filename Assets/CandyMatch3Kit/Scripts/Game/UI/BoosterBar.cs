fe83616e3e78e8cc7399d5e23edb11c179d81b04
Kuntay KOCA <kuntay.koca.development@gmail.com>
2023-03-31 23:02:31 +0300

    güncelleme uı falan

--- Assets/CandyMatch3Kit/Scripts/Game/UI/BoosterBar.cs
+++ Assets/CandyMatch3Kit/Scripts/Game/UI/BoosterBar.cs
@@ -25,27 +25,42 @@ namespace GameVanilla.Game.UI
 
 		[SerializeField]
 		private BuyBoosterButton button4;
+
+        [SerializeField]
+        private GameObject _lock1;
+
+        [SerializeField]
+        private GameObject _lock2;
+
+        [SerializeField]
+        private GameObject _lock3;
+
+        [SerializeField]
+        private GameObject _lock4;
 #pragma warning restore 649
 
-		/// <summary>
-		/// Sets the data of the in-game booster buttons.
-		/// </summary>
-		/// <param name="level">The current level.</param>
-		public void SetData(Level level)
+        /// <summary>
+        /// Sets the data of the in-game booster buttons.
+        /// </summary>
+        /// <param name="level">The current level.</param>
+        public void SetData(Level level)
 		{
 			if (level.availableBoosters[BoosterType.Lollipop])
 			{
 				button1.UpdateAmount(PlayerPrefs.GetInt("num_boosters_0"));
-			}
+                _lock1.SetActive(false);
+            }
 			else
 			{
 				button1.gameObject.SetActive(false);
+				
 			}
 
 			if (level.availableBoosters[BoosterType.Bomb])
 			{
 				button2.UpdateAmount(PlayerPrefs.GetInt("num_boosters_1"));
-			}
+                _lock2.SetActive(false);
+            }
 			else
 			{
 				button2.gameObject.SetActive(false);
@@ -54,7 +69,8 @@ namespace GameVanilla.Game.UI
 			if (level.availableBoosters[BoosterType.Switch])
 			{
 				button3.UpdateAmount(PlayerPrefs.GetInt("num_boosters_2"));
-			}
+                _lock3.SetActive(false);
+            }
 			else
 			{
 				button3.gameObject.SetActive(false);
@@ -63,7 +79,8 @@ namespace GameVanilla.Game.UI
 			if (level.availableBoosters[BoosterType.ColorBomb])
 			{
 				button4.UpdateAmount(PlayerPrefs.GetInt("num_boosters_3"));
-			}
+                _lock4.SetActive(false);
+            }
 			else
 			{
 				button4.gameObject.SetActive(false);
